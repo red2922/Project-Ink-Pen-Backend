45f3e422957845b99eeb4023cd3e4e77be2deaf6
Jake Inthavongsa <jinthavongsa1@gmail.com>
2025-03-15 22:06:22 -0500

    Added What I need to do

--- src/main/java/com/jake/demo/Controllers/CartController.java
+++ src/main/java/com/jake/demo/Controllers/CartController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,15 @@ public class CartController {
     private CartService cartService;
 
     @GetMapping("/cart/{id}")
-    Optional<Cart> getCartById(@PathVariable int id) {
+    public Optional<Cart> getCartById(@PathVariable int id) {
         return cartService.getCartById(id);
     }
+
+    @PostMapping("/cart/{id}")
+    public void processCart(@RequestBody Cart cart){
+        
+    }
+
+
 
 }
--- src/main/java/com/jake/demo/Services/CartService.java
+++ src/main/java/com/jake/demo/Services/CartService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jake.demo.Repository.CartRepository;
 import com.jake.demo.dto.Cart;
@@ -15,6 +16,12 @@ public class CartService {
 
     public Optional<Cart> getCartById(Integer id) {
         return cartRepository.findById(id);
+    }
+
+    @Transactional
+    public void processCart() {
+        //Process cart will first check if the cart exists. If null create a new cart otherwise
+        //go through every cart for an account. First subtracts the money from the current account. Then gives them to reflect both the posts and posters accounts. 
     }
 
 }
