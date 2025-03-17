bbdab8e4bb3df68516f9d6ecfffe53ac504514f3
Jake Inthavongsa <jinthavongsa1@gmail.com>
2025-03-17 14:31:53 -0500

    Almost done

--- src/main/java/com/jake/demo/Repository/PostsRepository.java
+++ src/main/java/com/jake/demo/Repository/PostsRepository.java
@@ -1,9 +1,14 @@
 package com.jake.demo.Repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 
 import com.jake.demo.dto.Post;
 
 public interface PostsRepository extends MongoRepository<Post, Integer> {
 
+    @Query("{ '_id': ?0 }")
+    @Update("{ '$inc': { balance: ?1 } }")
+    void changeBalance(Integer id, float pay);
 }
--- src/main/java/com/jake/demo/Services/CartService.java
+++ src/main/java/com/jake/demo/Services/CartService.java
@@ -2,24 +2,39 @@ package com.jake.demo.Services;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.jake.demo.Repository.CartRepository;
 import com.jake.demo.dto.Cart;
+import com.jake.demo.dto.Order;
 
 @Service
 public class CartService {
-    @Autowired
+
     private CartRepository cartRepository;
+    private CustomerService customerService;
+    private PostsService postsService;
+
+    public CartService(CartRepository cartRepository, CustomerService customerService, PostsService postsService){
+        this.cartRepository = cartRepository;
+        this.customerService = customerService;
+        this.postsService = postsService;
+    }
+
 
     public Optional<Cart> getCartById(Integer id) {
         return cartRepository.findById(id);
     }
 
     @Transactional
-    public void processCart() {
+    public void processCart(Cart cart) {
+        
+        for(Order order : cart.getTips()){
+            customerService.changeBalance(cart.getUserId(), -order.getTip());
+            customerService.changeBalance(order.getPost().getUserId(), order.getTip());
+            //Need to add an update to the post balance
+        }
+
         //Process cart will first check if the cart exists. If null create a new cart otherwise
         //go through every cart for an account. First subtracts the money from the current account. Then gives them to reflect both the posts and posters accounts. 
     }
